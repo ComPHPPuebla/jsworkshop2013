4585bfa598809130f26bfa3726c57b6d2abfa752
Luis Montealegre <luis.montealegre@mandragora-web-systems.com>
2013-11-20 00:40:23 -0600

    Agrego más ejemplos de estructuras de control.

--- control-structures.js
+++ control-structures.js
@@ -31,3 +31,57 @@ if (a > b) {
         max = c;
     }
 }
+
+// switch
+var level = 'expert';
+switch (level) {
+    case 'beginner':
+        // sentences
+        break;
+    case 'intermediate':
+        // sentences
+        break;
+    case 'advanced':
+        // sentences
+        break;
+    case 'expert':
+        //sentences
+        break;
+    default:
+        //sentences
+}
+
+// while
+var questionsCounter = 0;
+var totalQuestions = 10;
+while (questionsCounter < totalQuestions) {
+    // sentences
+    questionsCounter++;
+}
+
+// do-while
+var answer = false;
+do {
+    //sentences
+    answer = confirm('Do you want to continue?');
+} while (answer);
+
+// for
+var i;
+var count = 6;
+for (i = 0; i < count; i += 1) {
+  //sentences
+}
+
+// for in
+var property;
+for (property in user) {
+    document.writeln(property);
+}
+
+// try/catch
+try {
+    //sentences
+} catch(error) {
+    document.writeln(error.message);
+}
